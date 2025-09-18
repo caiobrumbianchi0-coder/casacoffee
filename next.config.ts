ce6e3977ce1686d3f5ab48bcab0f9233d125b4d7
caio bianchi <caiobrumbianchi0@gmail.com>
2025-09-18 20:05:03 +0000

    Initial commit: CasaCoffee

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = { output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
